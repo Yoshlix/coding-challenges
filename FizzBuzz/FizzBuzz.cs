ce9b75ab45ee8cfb55521e044ad1b1bf9dc7722f
Simon K. <sinos0807@gmail.com>
2024-07-24 11:27:51 +0200

    - first commit - fizz buzz challenge - title string challenge

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+
+namespace FizzBuzz
+{
+    public static class FizzBuzz
+    {
+
+        [Fact]
+        public static void FizzTest()
+        {
+            Assert.Equal(["1","2","Fizz"], Fizz_Buzz(3));
+        }
+
+        [Fact]
+        public static void FizzBuzzSuccess()
+        {
+            Assert.Equal(["1","2","Fizz","4","Buzz"], Fizz_Buzz(5));
+        }
+
+        [Fact]
+        public static void FizzBuzzFail()
+        {
+            Assert.NotEqual(["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","Fizz","14","FizzBuzz"], Fizz_Buzz(15));
+        }
+        
+        private static ArrayList Fizz_Buzz(int input)
+        {
+            var i = 0;
+            var inputArray = new ArrayList();
+            var outputArray = new ArrayList();
+
+            while (i != input)
+            {
+                i++;
+                inputArray.Add(i.ToString());
+
+            }
+
+            foreach (string number in inputArray)
+            {
+                switch (int.Parse(number) % 3)
+                {
+                    case 0 when int.Parse(number) % 5 == 0:
+                        outputArray.Add("FizzBuzz");
+                        break;
+                    case 0:
+                        outputArray.Add("Fizz");
+                        break;
+                    default:
+                    {
+                        outputArray.Add(int.Parse(number) % 5 == 0 ? "Buzz" : number.ToString());
+
+                        break;
+                    }
+                }
+            }
+
+            return outputArray;
+        }
+    }
+}
---
+++ TitleString/TitleString.cs
@@ -0,0 +1,25 @@
+﻿using static System.Char;
+
+namespace TitleString
+{
+    public static class TitleString
+    {
+        [Fact]
+        public static void CheckTitleTrue()
+        {
+            Assert.True(CheckTitle("A Mind Boggling Achievement"));
+            Assert.True(CheckTitle("A Simple C++ Program!"));
+        }
+
+        [Fact]
+        public static void CheckTitleFalse()
+        {
+            Assert.False(CheckTitle("Water is transparent"));
+        }
+
+        private static bool CheckTitle(string title)
+        {
+            return title.Split(' ').Count(word => IsUpper(word[0])) == title.Split(' ').Length;
+        }
+    }
+}
