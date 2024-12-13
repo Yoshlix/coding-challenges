b782a9d4999c7959e04e558935f33982200b22a2
Simon K. <75274054+Yoshlix@users.noreply.github.com>
2024-12-13 21:23:23 +0100

    Added new coding challenge

---
+++ PhoneNumber/PhoneNumber.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PhoneNumber;
+
+public class PhoneNumberProblem
+{
+    [Fact]
+    public void PhoneNumberTests()
+    {
+        Assert.Equivalent(IsFormattedPhoneNumber("(123) 456-7890"), true);
+        Assert.Equivalent(IsFormattedPhoneNumber("1111)555 2345"), false);
+        Assert.Equivalent(IsFormattedPhoneNumber("098) 123 4567"), false);
+    }
+
+    private static bool IsFormattedPhoneNumber(string phoneNumber)
+    {
+        return Regex.IsMatch(phoneNumber, "\\([0-9]+\\)\\s[0-9]+-[0-9]+");
+    }
+}
